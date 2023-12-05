c02e07ffdc8198d33491a93ce9811d190d7d889d
CaptenSepp <sepp.hashemi.it8@gmail.com>
2023-12-05 18:44:10 +0100

    code cleaned

--- game_project/lib/src/modules/home/components/qr_scan.dart
+++ game_project/lib/src/modules/home/components/qr_scan.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:async';
-import 'dart:developer';
+// import 'dart:developer';
 import 'dart:io';
 
 import 'package:flutter/foundation.dart';
