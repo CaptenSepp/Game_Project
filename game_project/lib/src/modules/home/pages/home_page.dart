ed427a4d1c4361ea0d26f9ee11b36cc9c964566f
sepp <sepp.hashemi.services@gmail.com>
2023-12-26 20:24:44 +0100

    added ColumnCountCalculator AnimationBG Gridviewbuilder adapted

--- game_project/lib/src/modules/home/pages/home_page.dart
+++ game_project/lib/src/modules/home/pages/home_page.dart
@@ -7,6 +7,8 @@ import 'package:game_project/src/constants/values.dart';
 import 'package:game_project/src/modules/home/components/image_show_widget.dart';
 import 'package:game_project/src/modules/home/components/qr_scan.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:animated_background/animated_background.dart';
+import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.title});
@@ -15,7 +17,7 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   // Variables to store barcode result and card flip information
 
   Barcode? currentBarcodeResult; // stores the barcode
@@ -25,12 +27,17 @@ class _HomePageState extends State<HomePage> {
   List<bool> frontSide = [false, false]; // front side which we see, false means question, true means Photo
   List<String> matches = [];
   bool _flipXAxis = false;
+  // created a methodd which calculates the number of rows and columns each time
+  int itemCount = 0;
+  int columnCount = 0;
 
-  // @override
-  // void initState() {
-  //   shuffleMapValues(qrToImageMap);
-  //   super.initState();
-  // }
+  @override
+  void initState() {
+    columnCountCalculator();
+    // shuffleMapValues(qrToImageMap);
+    super.initState();
+  }
+
   void restartAndShuffle() {
     showDialog(
       context: context,
@@ -162,7 +169,29 @@ class _HomePageState extends State<HomePage> {
       if (barcodeResult1?.code != null && barcodeResult2?.code != null) {
         matches.add(barcodeResult1!.code!);
         matches.add(barcodeResult2!.code!);
+
+        // to add to flipped and toShowlist Lists in values
+        flippedCards.add(barcodeResult1!.code!);
+        flippedCards.add(barcodeResult2!.code!);
+        toShowCards.remove(barcodeResult1!.code!);
+        toShowCards.remove(barcodeResult2!.code!);
+        columnCountCalculator();
       }
+    }
+  }
+
+  // here we calculate how many columns we need for flipped cardds
+  void columnCountCalculator() {
+    // here get checked how many cards are remaining unflipped
+    if (toShowCards.length <= 20 && toShowCards.length > 12) {
+      columnCount = 5;
+    } else if (toShowCards.length <= 12 && toShowCards.length > 6) {
+      columnCount = 4;
+    } else if (toShowCards.length <= 6 && toShowCards.length > 2) {
+      columnCount = 3;
+      // todo here it should be a error catcher which the numbers doesnt go something fully wrong
+    } else {
+      columnCount = 2;
     }
   }
 
@@ -181,8 +210,9 @@ class _HomePageState extends State<HomePage> {
   void whichCardMustFlip(Barcode? barcode) {
     setState(() {
       if (currentBarcodeResult?.code == barcode?.code) {
-        // we must flip, if new barcode is not the old barcode.
+        // todo we must flip, if new barcode is not the old barcode.
         return;
+        // todo we also must add: dont flip the card if its in the List of flipped cards in the map.dart
       }
       if (!frontSide[0]) {
         flipCardWithIndex(0);
@@ -216,69 +246,102 @@ class _HomePageState extends State<HomePage> {
         title: Text(widget.title),
         leading: IconButton(onPressed: restartAndShuffle, icon: const Icon(Icons.refresh_rounded)),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: <Widget>[
-            Expanded(
-              flex: 8,
-              child: Stack(
-                alignment: AlignmentDirectional.bottomCenter,
-                children: [
-                  Positioned.fill(
+      body: AnimatedBackground(
+        behaviour: RandomParticleBehaviour(
+          options: const ParticleOptions(
+            spawnMaxRadius: 80,
+            spawnMinRadius: 40,
+            spawnMaxSpeed: 250,
+            spawnMinSpeed: 100,
+            particleCount: 20,
+            maxOpacity: .8,
+            minOpacity: .5,
+
+            // spawnOpacity: 0.6,
+            // baseColor: Colors.red,
+            image: Image(image: AssetImage('assets/q.png')),
+          ),
+        ),
+        vsync: this as TickerProvider,
+        child: Center(
+          child: bodyCreator(),
+        ),
+      ),
+    );
+  }
+
+  Padding bodyCreator() {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          Expanded(
+            flex: 8,
+            child: Stack(
+              alignment: AlignmentDirectional.bottomCenter,
+              children: [
+                Positioned.fill(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
                     child: ClipRRect(
                       borderRadius: BorderRadius.circular(50),
-                      child: QRViewExample(
-                        flipCart: whichCardMustFlip,
-                        changeBarcodeResult: changeBarcodeResult,
+                      child: Opacity(
+                        opacity: 0.9,
+                        child: QRViewExample(
+                          flipCart: whichCardMustFlip,
+                          changeBarcodeResult: changeBarcodeResult,
+                        ),
                       ),
                     ),
                   ),
-                  Positioned(
-                    // bottom: 0,
-                    bottom: 10,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Opacity(
-                            opacity: 0.6,
-                            child: SizedBox(
-                              height: 100,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(40),
-                                child: _buildFlipAnimationAct(0),
-                              ),
+                ),
+                Positioned(
+                  // bottom: 0,
+                  bottom: 20,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(0.0),
+                        child: Opacity(
+                          opacity: 0.7,
+                          child: SizedBox(
+                            height: 150,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(40),
+                              child: _buildFlipAnimationAct(0),
                             ),
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.all(10.0),
-                          child: Opacity(
-                            opacity: 0.6,
-                            child: SizedBox(
-                              height: 100,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(40),
-                                child: _buildFlipAnimationAct(1),
-                              ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(0.0),
+                        child: Opacity(
+                          opacity: 0.7,
+                          child: SizedBox(
+                            height: 150,
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(40),
+                              child: _buildFlipAnimationAct(1),
                             ),
                           ),
                         ),
-                      ],
-                    ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-            Expanded(
-              flex: 3,
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 2,
+          ),
+          Expanded(
+            flex: 3,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: Opacity(
+                    opacity: 0.8,
                     child: Container(
                       decoration: BoxDecoration(
                         shape: BoxShape.rectangle,
@@ -286,13 +349,14 @@ class _HomePageState extends State<HomePage> {
                       ),
                       child: Center(
                         child: GridView.builder(
-                          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 5,
+                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: columnCount,
                             crossAxisSpacing: 1.0,
                             mainAxisSpacing: 1.0,
                           ),
-                          itemCount: 20, // TODO itemCount: itemCount, define up in the code
+                          itemCount: toShowCards.length, // TODO itemCount: itemCount, define up in the code
                           itemBuilder: (context, index) {
+                            int cardNumber = int.parse(toShowCards[index]);
                             return Padding(
                               padding: const EdgeInsets.all(3.0),
                               child: Container(
@@ -301,13 +365,14 @@ class _HomePageState extends State<HomePage> {
                                     BoxShadow(
                                         color: Colors.white.withOpacity(0.5), spreadRadius: 1, blurRadius: 3, offset: const Offset(0, 2))
                                   ],
-                                  color: getBackgroundColorMatches(index + 1),
+                                  // todo we dont need this anymore
+                                  color: getBackgroundColorMatches(cardNumber),
                                   borderRadius: BorderRadius.circular(15),
                                   border: Border.all(color: Colors.orange, width: 3.0),
                                 ),
                                 child: Center(
                                   child: Text(
-                                    '${index + 1}',
+                                    '${cardNumber}',
                                     style: const TextStyle(
                                       fontSize: 26.0,
                                       fontWeight: FontWeight.bold,
@@ -321,8 +386,11 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                   ),
-                  Expanded(
-                    flex: 1,
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Opacity(
+                    opacity: 0.8,
                     child: Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: Container(
@@ -336,7 +404,7 @@ class _HomePageState extends State<HomePage> {
                         ),
                         child: Center(
                           child: ListView.builder(
-                            itemCount: 8,
+                            itemCount: 4,
                             itemBuilder: (context, index) {
                               return Container(
                                 margin: const EdgeInsets.symmetric(vertical: 8.0),
@@ -346,7 +414,7 @@ class _HomePageState extends State<HomePage> {
                                 ),
                                 child: Center(
                                   child: Text(
-                                    '${index + 1}' + " Player",
+                                    '${index + 1}' " Player",
                                     style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
                                   ),
                                 ),
@@ -357,12 +425,26 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
+
+
+// Container(
+//             decoration: BoxDecoration(
+//               gradient: LinearGradient(
+//                 begin: Alignment.topLeft,
+//                 end: Alignment.bottomRight,
+//                 colors: [
+//                   Color(0xFF74EBD5),
+//                   Color(0xFFACD8AA),
+//                 ],
+//               ),
+//             ),
+//           ),
---
+++ game_project/lib/src/modules/home/pages/test.dart
@@ -0,0 +1,266 @@
+// import 'dart:ui';
+
+// import 'package:flutter/material.dart';
+
+// class Test extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         children: [
+//           Image.asset(
+//             'assets/bg.png', // Replace with the path to your image asset
+//             fit: BoxFit.cover,
+//             width: double.infinity,
+//             height: double.infinity,
+//           ),
+//           Center(
+//             child: ClipRRect(
+//               borderRadius: BorderRadius.circular(50.0),
+//               child: BackdropFilter(
+//                 filter: ImageFilter.blur(sigmaX: 3.0, sigmaY: 1.0),
+//                 child: Container(
+//                   width: 350.0,
+//                   padding: EdgeInsets.all(20.0),
+//                   decoration: BoxDecoration(
+//                     color: Colors.blue.withOpacity(0.2),
+//                     borderRadius: BorderRadius.circular(50.0),
+//                     boxShadow: [
+//                       BoxShadow(
+//                         color: Colors.black.withOpacity(0.4),
+//                         spreadRadius: 5,
+//                         blurRadius: 20,
+//                         offset: Offset(0, 3),
+//                       ),
+//                     ],
+//                   ),
+//                   child: Column(
+//                     mainAxisSize: MainAxisSize.min,
+//                     children: [
+//                       Text(
+//                         "Glassmorphism Test",
+//                         style: TextStyle(
+//                           fontSize: 24.0,
+//                           fontWeight: FontWeight.bold,
+//                           color: Colors.black87,
+//                         ),
+//                       ),
+//                       SizedBox(height: 20.0),
+//                       Text(
+//                         "A more complex layout with glassmorphism effect.",
+//                         style: TextStyle(fontSize: 16.0, color: Colors.black54),
+//                         textAlign: TextAlign.center,
+//                       ),
+//                       SizedBox(height: 20.0),
+//                       ElevatedButton(
+//                         onPressed: () {},
+//                         child: Text("Click Me"),
+//                         style: ElevatedButton.styleFrom(
+//                           backgroundColor: Colors.blue,
+//                           foregroundColor: Colors.white,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// import 'dart:ui';
+
+// import 'package:flutter/material.dart';
+
+// class Test extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         children: [
+//           // Background Image
+//           Image.asset(
+//             'assets/bg.png', // Replace with the path to your image asset
+//             fit: BoxFit.cover,
+//             width: double.infinity,
+//             height: double.infinity,
+//           ),
+//           // Glassmorphic Container
+//           Center(
+//             child: ClipRRect(
+//               borderRadius: BorderRadius.circular(20.0),
+//               child: BackdropFilter(
+//                 filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+//                 child: Container(
+//                   width: 350.0,
+//                   padding: EdgeInsets.all(20.0),
+//                   decoration: BoxDecoration(
+//                     color: Colors.white.withOpacity(0.3),
+//                     borderRadius: BorderRadius.circular(20.0),
+//                     boxShadow: [
+//                       BoxShadow(
+//                         color: Colors.black.withOpacity(0.2),
+//                         spreadRadius: 5,
+//                         blurRadius: 7,
+//                         offset: Offset(0, 3),
+//                       ),
+//                     ],
+//                   ),
+//                   child: Column(
+//                     mainAxisSize: MainAxisSize.min,
+//                     children: [
+//                       Text(
+//                         "Advanced Glassmorphism",
+//                         style: TextStyle(
+//                           fontSize: 24.0,
+//                           fontWeight: FontWeight.bold,
+//                           color: Colors.black87,
+//                         ),
+//                       ),
+//                       SizedBox(height: 20.0),
+//                       Text(
+//                         "A more advanced glassmorphic layout with a button.",
+//                         style: TextStyle(fontSize: 16.0, color: Colors.black54),
+//                         textAlign: TextAlign.center,
+//                       ),
+//                       SizedBox(height: 20.0),
+//                       // Glassmorphic Button
+//                       GlassmorphicButton(
+//                         onPressed: () {
+//                           // Handle button click
+//                         },
+//                         child: Text(
+//                           "Click Me",
+//                           style: TextStyle(
+//                             color: Colors.white,
+//                             fontWeight: FontWeight.bold,
+//                           ),
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+// // Glassmorphic Button Widget
+// class GlassmorphicButton extends StatelessWidget {
+//   final VoidCallback onPressed;
+//   final Widget child;
+
+//   GlassmorphicButton({required this.onPressed, required this.child});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       decoration: BoxDecoration(
+//         borderRadius: BorderRadius.circular(20.0),
+//         gradient: LinearGradient(
+//           begin: Alignment.topLeft,
+//           end: Alignment.bottomRight,
+//           colors: [
+//             Color(0xFF74EBD5),
+//             Color(0xFFACD8AA),
+//           ],
+//         ),
+//         boxShadow: [
+//           BoxShadow(
+//             color: Colors.white.withOpacity(0.2),
+//             spreadRadius: -2,
+//             blurRadius: 12,
+//             offset: Offset(0, 10),
+//           ),
+//           BoxShadow(
+//             color: Colors.black.withOpacity(0.3),
+//             spreadRadius: 2,
+//             blurRadius: 10,
+//             offset: Offset(0, 4),
+//           ),
+//         ],
+//       ),
+//       child: Material(
+//         color: Colors.transparent,
+//         borderRadius: BorderRadius.circular(20.0),
+//         child: InkWell(
+//           onTap: onPressed,
+//           borderRadius: BorderRadius.circular(20.0),
+//           child: Container(
+//             padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+//             child: Center(child: child),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+import 'package:flutter/material.dart';
+import 'package:glassmorphism/glassmorphism.dart';
+
+class Test extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Modern Advanced Layout with Glassmorphism'),
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            // Background image
+            Container(
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/bg.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+
+            Center(
+              child: GlassmorphicContainer(
+                width: 70,
+                height: 70,
+                borderRadius: 10,
+                blur: 5,
+                alignment: Alignment.bottomCenter,
+                border: 0,
+                linearGradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color.fromARGB(255, 98, 41, 230).withOpacity(0.3),
+                    Color(0xFFFFFFFF).withOpacity(0.0),
+                  ],
+                  stops: const [
+                    0.1,
+                    1,
+                  ],
+                ),
+                borderGradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xFFffffff).withOpacity(0.15),
+                    Color((0xFFFFFFFF)).withOpacity(0.9),
+                  ],
+                ),
+                child: null,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
