09ec4051b875149eeacef66ead7e91c2f5f361b9
sepp <sepp.hashemi.services@gmail.com>
2023-12-28 13:10:30 +0100

    ignore use key in widget added to files

--- game_project/lib/src/modules/home/components/image_show_widget.dart
+++ game_project/lib/src/modules/home/components/image_show_widget.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
 import 'package:game_project/src/constants/map.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class ImageShowWidget extends StatelessWidget {
+  // ignore: annotate_overrides, overridden_fields
   final Key key;
   final bool showImage;
   final Barcode? barcodeResult;
